8d348e6a0736a840b4b21b72303e583a8725c4fb
F88 <685250+F88@users.noreply.github.com>
2025-12-06 17:13:05 +0900

    Add GitHub Actions workflow for deployment and update package configuration
    - Create a GitHub Actions workflow for deploying to GitHub Pages on push to the main branch.
    - Add `gh-pages` as a dependency in package.json for deployment.
    - Update package.json scripts to include predeploy step for building the project before deployment.
    - Modify vite.config.ts to ensure the base path is set correctly for GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/PROMIDAS-demo/",
   plugins: [react()],
+  // @ts-expect-error - Vitest config
   test: {
     globals: true,
     environment: "jsdom",
